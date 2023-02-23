323c0045bc4f51df93dd4124fb071e764360dab3
ojkamson <100039112+ojkamson@users.noreply.github.com>
2023-02-23 07:58:23 +0100

    Add files via upload

---
+++ sendMail.php
@@ -0,0 +1,13 @@
+<?php
+
+//------------Note You must use paid cpanel hosting ---------------
+
+$domainName = "website.com"; // Enter Domain Name Here
+$webmail ="info@website.com"; // Enter Webmail Email
+$webmailPass = "123456";  //Enter Webmail Password
+$recieverMail ="test@gmail.com"; // Your reciever email address
+
+
+//----------------------------------
+
+?>
